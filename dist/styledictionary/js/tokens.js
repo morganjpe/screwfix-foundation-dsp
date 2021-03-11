23be284342c05ca9273b6508a99c3d59bb6802d8
Evans <evansm54@GBSFYVL001M7922.local>
2021-03-11 09:27:26 +0000

    test

--- dist/styledictionary/js/tokens.js
+++ dist/styledictionary/js/tokens.js
@@ -1,9 +1,9 @@
 /**
  * Do not edit directly
- * Generated on Thu, 11 Mar 2021 07:39:19 GMT
+ * Generated on Thu, 11 Mar 2021 09:26:58 GMT
  */
 
-export const SfFoundationGreen = "#51ae32";
+export const SfFoundationGreen = "#9632ae";
 export const SfBlue = "#0053a0";
 export const SfFoundationLightgrey = "#949494";
 export const SfRed = "#ed1c24";
